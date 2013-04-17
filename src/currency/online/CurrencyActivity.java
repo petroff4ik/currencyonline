fe1d48863daea1d8aa01c55426ba95008301f791
Andrey Petrov <petroff@favoritbet.com>
2013-04-17 17:48:47 +0300

    add service

--- src/currency/online/CurrencyActivity.java
+++ src/currency/online/CurrencyActivity.java
@@ -1,15 +1,17 @@
 package currency.online;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class CurrencyActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+public class CurrencyActivity extends Activity {
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		startService(new Intent(this, CurrencyService.class));
+	}
 }
